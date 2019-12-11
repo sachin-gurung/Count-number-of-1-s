b15e6196d506fecef2bd3e2a0c1fedb5804ec83c
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:36:33 -0800

    Add project files.

---
+++ Count number of 1's/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_number_of_1_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
